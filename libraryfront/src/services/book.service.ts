1368e8875d3fdebad26f158ecdbc37a153f87402
Nicolas Da Silva <43333430+ndasilva1408@users.noreply.github.com>
2020-05-05 14:38:52 +0200

    MàJ ajout billet

--- libraryfront/src/services/book.service.ts
+++ libraryfront/src/services/book.service.ts
@@ -42,8 +42,9 @@ export class BookService {
     updateBook(form: FormGroup): Observable<Book> {
         return this.http.put<Book>(this.bookURL + '/updateBook', form.value);
     }
-    updateBookQty(form: FormGroup): Observable<Book> {
-        return this.http.put<Book>(this.bookURL + '/updateBookQty', form.value);
+
+    updateBookQty(idBook: any): Observable<Book> {
+        return this.http.put<Book>(this.bookURL + '/updateBookQty', {params: new HttpParams().set('id', idBook)});
     }
 
     updateBookStatus(book: Book): Observable<Book> {
